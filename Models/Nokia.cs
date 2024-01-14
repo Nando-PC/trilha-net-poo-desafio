5316d14cdb29403d7291a554c84a4800f644fbe4
Fernando Rodrigues Silva <77867538+Nando-PC@users.noreply.github.com>
2024-01-14 16:00:15 -0300

    Desafio DotNet POO concluído

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    //implementado
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {}
+        
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        //implementado
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o Aplicativo {nomeApp} para o Nokia.");
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+//implementado
+
+Console.WriteLine("Smartphone: Iphone");
+Iphone cel1 = new Iphone("967153045", "IphoneS", "45fert", 16);
+cel1.InstalarAplicativo("Waze");
+cel1.Ligar();
+
+Console.WriteLine("-----------------------");
+
+Console.WriteLine("Smartphone: Nokia");
+Nokia cel2 = new Nokia("922153044", "Nokia-9", "88bart", 16);
+cel2.InstalarAplicativo("Facebook");
+cel2.ReceberLigacao();
